29c7dda55d789ab8af830d47ca24e2dbf9fa6310
abhignakumar <abhigna728@gmail.com>
2026-06-07 12:10:26 -0400

    Implemented calculateSpringSettlingDurationMs function

--- src/main/lib/config.ts
+++ src/main/lib/config.ts
@@ -1,1 +1,9 @@
 export const projectsRootDir = '~/Open Studio Projects';
+
+export const defaultSpringAnimationConfig = {
+  screenMovement: {
+    mass: 2.25,
+    stiffness: 200,
+    damping: 40,
+  },
+};
--- src/main/lib/constants.ts
+++ src/main/lib/constants.ts
@@ -1,7 +1,14 @@
+import { defaultSpringAnimationConfig } from './config';
+import { calculateSpringSettlingDurationMs } from './utils';
+
 export const CLOSE_CURRENT_WINDOW = 'close-current-window';
 export const LIST_DISPLAY_SOURCES = 'list-display-sources';
 
 export const STOP_RECORDING = 'stop-recording';
 
-export const ZOOM_TRANSITION_TIME_MS = 2000;
+export const ZOOM_TRANSITION_TIME_MS = calculateSpringSettlingDurationMs(
+  defaultSpringAnimationConfig.screenMovement,
+);
 export const GROUP_CLICKS_WITHIN_TIME_MS = 3000;
+
+export const SPRING_SETTLING_TIME_TOLERANCE = 0.0001;
